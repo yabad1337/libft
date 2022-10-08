46ececb3f38073c7405b626b1e2bad06cef4bbab
Yahya Abad <yabad@c2r5p7.1337.ma>
2022-10-08 11:31:17 +0100

    strtrim update OK

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,75 +6,50 @@
 /*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 09:21:07 by yabad             #+#    #+#             */
-/*   Updated: 2022/10/05 13:30:44 by yabad            ###   ########.fr       */
+/*   Updated: 2022/10/08 11:29:45 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_remove_doublons(char const *set) //correct set
-{
-	char	*new_set;
-	int		t[127] = {0};
-	int		i;
-	int		j;
-	int		c;
-
-	i = -1;
-	c = 0;
-	while (set[++i])
-	{
-		if (t[(int)set[i]] == 0)
-		{
-			t[(int)set[i]] = 1;
-			c++;
-		}
-	}
-	new_set = (char *)malloc(c * sizeof(char) + 1);
-	i = 0;
-	j = 0;
-	while (i < 127)
-	{
-		if (t[i] == 1)
-		{
-			new_set[j] = (char)i;
-			j++; 
-		}
-		i++;
-	}
-	new_set[j] = '\0';
-	return (new_set);
-}
-
-static int	ft_n_of_char(char const *s1, char const *set) //correct
+int		if_exist(char c, char const *set)
 {
 	int	i;
-	int	j;
-	int	c;
 
 	i = 0;
-	c = 0;
-	set = ft_remove_doublons(set);
 	while (set[i])
 	{
-		j = 0;
-		while (s1[j])
-		{
-			if (set[i] == s1[j])
-				c++;
-			j++;
-		}
+		if (set[i] == c)
+			return (1);
 		i++;
 	}
-	return (c);
+	return (0);
+}
+
+void	positions(char const *s1, char const *set, int *r, int *l)
+{
+	*r = 0;
+	*l = (int)ft_strlen(s1) - 1;
+	while (if_exist(s1[(*r)], set))
+		(*r)++;
+	while (if_exist(s1[(*l)], set))
+		(*l)--;
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	int	r;
+	int	l;
+
+	if (!s1)
+		return (NULL);
+	r = 0;
+	l = 0;
+	positions(s1, set, &r, &l);
+	return (ft_substr(s1, r, l - r + 1));
 }
 
-int	main()
-{
-	printf("%s\n", ft_remove_doublons("yyyyya"));
-}
+// int	main()
+// {
+// 	ft_strtrim("nbnlolobnbtonbn", "nbn");
+// }
