f7c4cfe14a4afecc05075e32ec10d1c4de8e9667
Yahya Abad <yabad@c2r5p7.1337.ma>
2022-10-09 17:37:30 +0100

    strnstr algorithm with implementation

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,50 +6,53 @@
 /*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 10:30:30 by yabad             #+#    #+#             */
-/*   Updated: 2022/10/06 10:50:14 by yabad            ###   ########.fr       */
+/*   Updated: 2022/10/09 16:53:18 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	match(const char *str, const char *needle, int p)
-{
-	int	i;
-
-	i = 0;
-	while (needle[i])
-	{
-		if (str[p + i] != needle[i])
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char    *ft_strnstr(const char *haystack, const char *needle, size_t n)
 {
 	size_t	i;
-	size_t	m;
+	size_t	j;
 
 	i = 0;
-	while (haystack[i] && i < len)
-	{
-		m = match(haystack, needle, i);
-		if (m == 1)
-			return ((char *)haystack + i);
-		i++;
-	}
-	if (needle[0] == '\0')
+	j = 0;
+	if (!haystack && n == 0)
+		return (NULL);
+	if (needle[i] == '\0')
 		return ((char *)haystack);
-	return (0);
+	while (haystack[i] && i < n)
+	{
+		if (!(haystack[i] == needle[j]))
+			i++;
+		else
+		{
+			if (needle[j + 1] && haystack[i + 1] == needle[j + 1])
+			{
+				j++;
+				i++;
+			}
+			else if (needle[j + 1] && haystack[i + 1] != needle[j + 1])
+			{
+				i = i - (j - 1);
+				j = 0;
+			}
+			else
+				return ((char *)haystack + (i - j));
+		}
+	}
+	return (NULL);
 }
 
-
-// int	main()
+// int main()
 // {
-// 	char haystack[30] = "aaabcabcd";
-// 	//char needle[10] = "aabc";
-
-// 	printf("%s\n", ft_strnstr(haystack, "cd", 8));
-// 	printf("%s\n", strnstr(haystack, "cd", 8));
+//     char 	s1[10] = "aaaabcabcd";
+// 	char 	s2[10] = "aaabc";
+// 	size_t	n = 9;
+ 
+//     printf("%s#\n", ft_strnstr(s1, s2, n));
+//     printf("%s#\n", strnstr(s1, s2, n));
+//     return (0);
 // }
