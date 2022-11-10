543a927875adec44b79de3f76928b257dc3cc6d3
Yahya Abad <yabad@c2r2p9.1337.ma>
2022-11-10 11:28:56 +0100

    nothing to protect, just made some changes to short the code

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 14:46:41 by yabad             #+#    #+#             */
-/*   Updated: 2022/10/14 16:04:33 by yabad            ###   ########.fr       */
+/*   Updated: 2022/11/10 11:26:33 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	t_list	*tmp;
 	int		i;
 
 	i = 0;
-	tmp = lst;
-	while (tmp != NULL)
-	{
-		i++;
-		tmp = tmp->next;
-	}
+	while (lst != NULL && ++i)
+		lst = lst->next;
 	return (i);
 }
+/*
+	INPUT: 	t_list	*lst 	---> (list, !list)
+	in worst case there nothing to protect since 
+	it will return 0 which means theres no list to count from
+*/
