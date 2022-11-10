6038a772e48c63e161535281e7d13bf56bbfae0a
Yahya Abad <yabad@c2r2p9.1337.ma>
2022-11-10 12:57:08 +0100

    handling all cases with free tests and tried to access memory after free in diffrent OS

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yabad <yabad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 11:24:49 by yabad             #+#    #+#             */
-/*   Updated: 2022/10/14 11:44:33 by yabad            ###   ########.fr       */
+/*   Updated: 2022/11/10 12:56:08 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,23 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if (!del)
-		return ;
-	if (lst)
+	if (del && lst)
 	{
 		del(lst->content);
 		free(lst);
 	}
 }
+/*
+	INPUT: 	t_list	*lst 	---> (List, NULL)
+			void (*del)		---> (exist, !exist)
+	the function to delete has to be present
+	and the node has to be present so we can delete it
+	otherwise function exit
+	//After freing the first element of a node, \
+			if you tried to access the 2nd element
+			it should segfault.
+	in school iMac : 	no segfault, you can access and \
+						modify the memory after free(WEIRD)
+	in debian : 	SEGFAULT
+	in mac air : 	SEGFAULT
+*/
